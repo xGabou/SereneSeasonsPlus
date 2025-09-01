7f0d3f6b01dd2d58d931ab1f519378f3698356f8
Gabriel Galipeau <gabgalip@gmail.com>
2025-09-01 13:22:56 -0400

    Refactor build configuration for Fabric compatibility and add initial mod files

---
+++ src/client/java/net/Gabou/sereneseasonsplus/client/SereneSeasonsPlusClient.java
@@ -0,0 +1,11 @@
+package net.Gabou.untitled.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UntitledClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
